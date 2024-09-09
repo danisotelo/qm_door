6fc3bb90bbb41bd7e519899cc05af61333a4938b
Daniel Sotelo <danisotacam@gmail.com>
2024-09-09 17:36:03 +0200

    :bulb: COM: Clarify StartingPosition.h entries

--- qm_controllers/include/qm_controllers/StartingPosition.h
+++ qm_controllers/include/qm_controllers/StartingPosition.h
@@ -6,14 +6,14 @@
 
 namespace qm {
 
-    constexpr double X = -2;
-    constexpr double Y = 0; // 1
-    constexpr double PSI_DEG = 0; // -40
-    constexpr double PSI = PSI_DEG * (M_PI / 180);
-    constexpr double ARM_DIST = 0.6;
-    constexpr double HEIGHT = 0.4;
-    constexpr double ARM_HEIGHT = 0.036;
-    constexpr double THRESHOLD = 0.1;
+    constexpr double X = -2; // Initial X position
+    constexpr double Y = 0; // Initial Y position
+    constexpr double PSI_DEG = 0; // Initial yaw angle in degrees
+    constexpr double PSI = PSI_DEG * (M_PI / 180); 
+    constexpr double ARM_DIST = 0.6; // Base CoM to End-effector distance in the XY plane
+    constexpr double HEIGHT = 0.4; // Base CoM height (should be the same as comHeight in reference.info)
+    constexpr double ARM_HEIGHT = 0.036; // Height difference between end-effector and base CoM
+    // constexpr double THRESHOLD = 0.1;
 
 } // namespace qm
 
